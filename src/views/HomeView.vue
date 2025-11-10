4f97cda1113170eddef13c41f6ec73af25e2f92d
yjsong2154 <yjsong2153@gmail.com>
2025-11-10 16:56:26 +0900

    Fix : Fix type bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <!-- src/views/HomeView.vue -->
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useBookStore } from '@/stores/book'
 import BookRail from '@/components/home/BookRail.vue'
 import Modal from '@/components/ui/Modal.vue'
@@ -10,9 +10,9 @@ import PersonalSuggestCard from '@/components/home/PersonalSuggestCard.vue'
 
 const store = useBookStore()
 
-// 예시로 첫 2개를 추천 카드에 사용 (원하면 서버/알고리즘 결과로 교체)
-const todayBook = () => store.list[0]
-const personalBook = () => store.list[1]
+// 존재 보장용 computed
+const firstBook  = computed(() => store.list.length >= 1 ? store.list[0] : null)
+const secondBook = computed(() => store.list.length >= 2 ? store.list[1] : null)
 
 const selectedId = ref<string | null>(null)
 const openModal = (id: string) => (selectedId.value = id)
@@ -21,17 +21,24 @@ const closeModal = () => (selectedId.value = null)
 
 <template>
   <main class="mx-auto max-w-6xl px-4 space-y-8">
-    <!-- 상단 추천 카드 2종 -->
-    <TodayPickCard v-if="store.list.length" :book="todayBook()" @open="openModal" />
-    <PersonalSuggestCard v-if="store.list.length > 1" :book="personalBook()" username="누워엄" @open="openModal" />
+    <!-- v-if로 존재 보장 후 non-null 단언으로 전달 -->
+    <TodayPickCard
+      v-if="firstBook"
+      :book="firstBook!"
+      @open="openModal"
+    />
+    <PersonalSuggestCard
+      v-if="secondBook"
+      :book="secondBook!"
+      username="누워엄"
+      @open="openModal"
+    />
 
-    <!-- 기존 레일들 -->
     <BookRail title="요즘 뜨는" />
     <BookRail title="장르별 추천" />
     <BookRail title="에디터 픽" />
   </main>
 
-  <!-- 모달 -->
   <Modal v-if="selectedId" @close="closeModal">
     <BookDetail :id="selectedId!" />
   </Modal>
